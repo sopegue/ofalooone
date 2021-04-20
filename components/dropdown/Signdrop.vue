09912fdda5861e08805ba45c26b82d707cfdfdc3
sopegue <yayasopeguesoro@gmail.com>
2021-04-20 03:41:27 +0100

    wip

--- components/dropdown/Signdrop.vue
+++ components/dropdown/Signdrop.vue
@@ -48,7 +48,7 @@
             <h4 class="font-semibold cursor-default size-15 logo-color px-4">
               Bienvenue,
               <span class="font-semibold size-15 logo-color">
-                {{ user.name | capitalize }} !
+                {{ user | capitalize }} !
               </span>
             </h4>
           </div>

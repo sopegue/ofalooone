4097c3d3ca09dbb7ff82eae9d8d2158459e22444
sopegue <yayasopeguesoro@gmail.com>
2021-04-22 01:50:33 +0100

    wip

--- components/dropdown/Signdrop.vue
+++ components/dropdown/Signdrop.vue
@@ -106,16 +106,18 @@
               ></path></svg
             ><span>Enregistrés</span></a
           >
-          <div v-show="$auth.loggedIn">
-            <hr class="dropdown-divider" />
-            <a class="block w-fit m-0-auto" @click="logout"
-              ><button
-                class="button is-light ferfe rounded px-20 py-1 text-white size-14"
+          <client-only>
+            <div v-show="$auth.loggedIn">
+              <hr class="dropdown-divider" />
+              <a class="block w-fit m-0-auto" @click="logout"
+                ><button
+                  class="button is-light ferfe rounded px-20 py-1 text-white size-14"
+                >
+                  Se deconnecter
+                </button></a
               >
-                Se deconnecter
-              </button></a
-            >
-          </div>
+            </div>
+          </client-only>
         </div>
       </div>
     </div>

4d1e0ef67f00e49137288ce1809c230ad987dfcf
sopegue <yayasopeguesoro@gmail.com>
2021-05-25 23:52:50 +0100

    bfdb

--- components/report/Report.vue
+++ components/report/Report.vue
@@ -7,17 +7,28 @@
         <div class="border-b pb-3">
           <h4
             v-if="!success"
-            class="font-semibold size-18 logo-color text-center"
+            class="font-semibold flex items-center justify-center space-x-2 size-18 logo-color text-center"
           >
-            Signalement
+            <svg
+              class="w-5 h-5 mt-0.5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9"
+              ></path>
+            </svg>
+            <span class="size-18 font-semibold logo-color">Signalement</span>
           </h4>
           <h4
             v-if="success"
             class="font-semibold flex items-center justify-center space-x-2 size-18 logo-color text-center"
           >
-            <span class="size-18 font-semibold logo-color"
-              >Merci pour votre signalement</span
-            >
             <svg
               class="w-5 h-5 mt-0.5"
               fill="none"
@@ -32,6 +43,9 @@
                 d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               ></path>
             </svg>
+            <span class="size-18 font-semibold logo-color"
+              >Merci pour votre signalement</span
+            >
           </h4>
         </div>
         <div v-if="error" class="w-full top-0 appearZ pt-3 px-5">
